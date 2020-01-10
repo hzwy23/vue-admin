bf8bf38b1d8a5649db27a9cd8224f4494b02a0ed
hzwy23 <hzwy23@163.com>
2020-01-11 01:10:36 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    devServer: {
+        port: 8080,
+        proxy: {
+            '/': {
+                // process.env.VUE_APP_BASE_API 替换成对应的后台服务地址
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    '^/': '/'
+                }
+            }
+        }
+    }
+}
